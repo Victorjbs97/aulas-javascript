2cc8d4c081f5f49a3161576f6386e3c7ea136ce7
Victorjbs97 <Victorjbs97@gmail.com>
2025-07-30 17:28:16 -0300

    Primeiras exercicios em array.

--- js/03-arrays.js
+++ js/03-arrays.js
@@ -1,1 +1,5 @@
 'use strict';
+let vocalistas = ["Ozzy","Dio","Lemmy","Jimmy hendrix","Joelma","Manoel gomes","David Gilmour"];
+console.log(`${vocalistas[0]}: Lendario vocalista, um dos criadores do Hevy Metal`);
+console.log(vocalistas[5]+ " Vocalista do sucesso caneta azul.");
+

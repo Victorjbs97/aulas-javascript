73200acb985c0f7a5f302a39ed607b9413f83b15
Victorjbs97 <Victorjbs97@gmail.com>
2025-07-30 15:23:21 -0300

    Exercicio com variaveis e console.

--- js/02-meu-script.js
+++ js/02-meu-script.js
@@ -1,2 +1,22 @@
-console.log(`Hello world`);
+/*
+    EXERCICIOS 
 
+    1) Crie uma variável chamada "uc" e coloque nela o número da UC que estamos
+
+    2) Crie uma variável chamada "competencia" e coloque nela uma frase que represente o que estamos estudando nesta UC.
+
+    3) Monte e mostre uma frase no console exibindo a UC e a competência. Exemplo: estamos na UC2 estudando Front-end com javaScript.
+
+*/
+
+// 1
+
+let uc = 2;
+
+// 2
+
+let competencia = "estamos na UC"+uc+" estudando Front-end com javaScript";
+
+// 3 
+
+console.log(competencia);

8388acdd3fc4745cd98f60b03a62a0b95f1d9ee6
Victorjbs97 <Victorjbs97@gmail.com>
2025-08-06 14:33:32 -0300

    Array com dados diversos

--- js/03-arrays.js
+++ js/03-arrays.js
@@ -3,3 +3,13 @@ let vocalistas = ["Ozzy","Dio","Lemmy","Jimmy hendrix","Joelma","Manoel gomes","
 console.log(`${vocalistas[0]}: Lendario vocalista, um dos criadores do Hevy Metal`);
 console.log(vocalistas[5]+ " Vocalista do sucesso caneta azul.");
 
+// Em js você pode colocar qualquer coisa em um array.
+
+let seila = 6;
+const coisas=[10,"Senac","<h2>Oie</h2>",seila, 15.88];
+
+console.log(coisas[4]);
+console.log(coisas[1]);
+
+
+

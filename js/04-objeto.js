2171c2d2b4b4ad8b5b2c199dc7a6e36c27431b66
Victorjbs97 <Victorjbs97@gmail.com>
2025-08-06 16:31:24 -0300

    Update 04-objeto.js

--- js/04-objeto.js
+++ js/04-objeto.js
@@ -1,1 +1,12 @@
 'use strict';
+/// Exemplo 1: Objetos com dados de uma pessoa
+
+const pessoa = {
+    nome:'chaves',
+    idade:8,
+    cidade:'São Paulo',
+    estado:'SP'
+};
+
+///Acessando as propreidades do objeto
+console.log(`O ${pessoa.nome} tem ${pessoa.idade} anos.`);

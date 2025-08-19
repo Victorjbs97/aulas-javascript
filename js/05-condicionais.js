e1c796a8b521cf6b77f3d49c592bdd10112e8e8e
Victorjbs97 <Victorjbs97@gmail.com>
2025-08-19 14:36:10 -0300

    começo de condicionais

---
+++ js/05-condicionais.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/* Comandos condicionais mais comum:
+
+if -se
+
+else - se não
+
+*/
+
+//Exemplos
+
+console.log("exemplo");
+
+let numero= 5;
+
+//condicional simples
+
+if(numero!=0){
+    console.log(numero);
+}

e5b61aa5db962570dfc09338d07ee24814514a4c
Victorjbs97 <Victorjbs97@gmail.com>
2025-08-19 17:08:13 -0300

    Operador ||

--- js/06-operadores_logicos.js
+++ js/06-operadores_logicos.js
@@ -26,4 +26,25 @@ if(idade>=18&&temCarta===true){
     com dois ==, se comprar o valor não importa o tipo exemplo: 10 inteiro e '10' string, a comparação é positiva
     com três ===, se compara o valor e o tipo. exemplo: 10 inteiro e '10' string, a comparação é false
 
+    0 . 0 = 0                     0 + 0 = 0             
+    0 . 1 = 0                     0 + 1 = 1
+    1 . 0 = 0                     1 + 0 = 1
+    1 . 1 = 1                     1 + 1 = 1
+
+    ordem de precedencia
+    !
+    &&
+    ||
 */
+
+
+console.log("\n\nExemplo ||")
+let feriado = false, fimdeSemana = false;
+
+if (feriado || fimdeSemana) {
+    console.log("\nNão tem aula!");
+    
+} else {
+    console.log("\nTem aula!");
+    
+}

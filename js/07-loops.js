80261a389540d3be760b320662692eb0f528b442
Victorjbs97 <Victorjbs97@gmail.com>
2025-08-20 13:57:10 -0300

    Loops

---
+++ js/07-loops.js
@@ -0,0 +1,11 @@
+'use strict';
+    /*
+        Loops ou estruturas de repetição
+        
+        São comandos que, baseados em alguma condição lógica, são progrmas para executar ações repetidas vezes.
+
+        Comandos maais tradicionais: while e for.
+
+        Normalmente, os loops são controlados através de uma variável contadora
+     */    
+    

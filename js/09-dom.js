b7b4cfa2afbaf4f8e0a25501cc502ada8cae44f9
Victorjbs97 <Victorjbs97@gmail.com>
2025-08-27 14:01:42 -0300

    Notelist e seleção de todos os elementos

--- js/09-dom.js
+++ js/09-dom.js
@@ -10,3 +10,9 @@ const pagina = document.querySelector("body");
 console.log(exemplo01);
 console.log(mensagem01);
 console.log(pagina);
+
+// querySelectorAll para selecionar todos os elementos.
+//NodeList
+
+const paragrafos = document.querySelectorAll("p");
+console.log(paragrafos);
